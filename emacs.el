e780b7cf0ccec9a488484c581828d8cdaca08437
Alexandru Nedelcu <noreply@alexn.org>
2011-06-29 08:57:36 +0300

    newest changes - added coffee / csharp

--- emacs.el
+++ emacs.el
@@ -13,13 +13,15 @@
 (load-file "~/.emacs.d/customizations.el")
 
 (load-file "~/.emacs.d/my-packages/yasnippets/load.el")
-(load-file "~/.emacs.d/my-packages/nxhtml-mode/load.el")
 (load-file "~/.emacs.d/my-packages/flymake-mode/load.el")
+(load-file "~/.emacs.d/my-packages/csharp-mode/load.el")
 (load-file "~/.emacs.d/my-packages/python-mode/load.el")
 (load-file "~/.emacs.d/my-packages/yaml-mode/load.el")
 (load-file "~/.emacs.d/my-packages/javascript-mode/load.el")
 (load-file "~/.emacs.d/my-packages/ruby-mode/load.el")
 (load-file "~/.emacs.d/my-packages/rails-mode/load.el")
 (load-file "~/.emacs.d/my-packages/django/load.el")
+(load-file "~/.emacs.d/my-packages/coffee-mode/load.el")
+(load-file "~/.emacs.d/my-packages/nxhtml-mode/load.el")
 
 
---
+++ my-packages/coffee-mode/load.el
@@ -0,0 +1,12 @@
+(add-to-list 'load-path "~/.emacs.d/my-packages/coffee-mode")
+(require 'coffee-mode)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
