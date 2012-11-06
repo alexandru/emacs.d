b0544c1e9ba594fda8dcc9f514777f1d124f3d6d
Alexandru Nedelcu <noreply@alexn.org>
2012-11-06 10:35:26 +0200

    Fixed coffee-mode

--- my-packages/clojure-mode/load.el
+++ my-packages/clojure-mode/load.el
@@ -10,6 +10,7 @@
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
 
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
 
 ;(defun na-load-buffer ()
 ;  (interactive)
--- my-packages/coffee-mode/load.el
+++ my-packages/coffee-mode/load.el
@@ -6,7 +6,9 @@
 
 (defun coffee-custom ()
   "coffee-mode-hook"
- (set (make-local-variable 'tab-width) 2))
+  (setq whitespace-action '(auto-cleanup)) ;; automatically clean up bad whitespace
+  (setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
+  (whitespace-mode))
 
 (add-hook 'coffee-mode-hook
   '(lambda() (coffee-custom)))
