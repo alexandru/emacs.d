a7ad4aecd096866b78dab1b13d17a008b2f39d42
Alexandru Nedelcu <noreply@alexn.org>
2015-09-19 16:00:38 +0300

    Get rid of ensime

--- settings/scala-mode2.el
+++ settings/scala-mode2.el
@@ -1,17 +1,18 @@
 (defvar scala-packages '(scala-mode2
-                         ensime
-                         exec-path-from-shell))
+                         exec-path-from-shell
+                         flycheck))
 
 (dolist (p scala-packages)
   (unless (package-installed-p p)
     (package-install p)))
 
 (setenv "PATH" (concat "/Users/alex/bin:" (getenv "PATH")))
-(setenv "PATH" (concat "/Users/alex/bin/scala-2.11.4/bin:" (getenv "PATH")))
+(setenv "PATH" (concat "/Users/alex/bin/scala-2.11.7/bin:" (getenv "PATH")))
 
 (when (memq window-system '(mac ns))
     (exec-path-from-shell-initialize))
 
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-
+;(require 'ensime)
+(add-hook 'scala-mode-hook 'flycheck-mode)
+;(add-hook 'scala-mode-hook #'yas-minor-mode)
+;(define-key company-active-map [tab] nil)
