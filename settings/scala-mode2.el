f6ba9cdeb75b3795df42bf98abe5759872778e48
Alexandru Nedelcu <noreply@alexn.org>
2016-05-24 16:52:28 +0300

    Fix scala-mode2 config

--- settings/scala-mode2.el
+++ settings/scala-mode2.el
@@ -1,13 +1,13 @@
-(defvar scala-packages '(scala-mode2
+(defvar scala-packages '(scala-mode
                          exec-path-from-shell))
 
 (dolist (p scala-packages)
   (unless (package-installed-p p)
     (package-install p)))
 
-(setenv "PATH" (concat "/Users/alex/bin:" (getenv "PATH")))
-(setenv "PATH" (concat "/Users/alex/bin/scala-2.11.7/bin:" (getenv "PATH")))
-(when (memq window-system '(mac ns)) (exec-path-from-shell-initialize))
+;(setenv "PATH" (concat "/Users/alex/bin:" (getenv "PATH")))
+;(setenv "PATH" (concat "/Users/alex/bin/scala-2.11.7/bin:" (getenv "PATH")))
+;(when (memq window-system '(mac ns)) (exec-path-from-shell-initialize))
 
 (add-hook 'scala-mode-hook #'yas-minor-mode)
 
