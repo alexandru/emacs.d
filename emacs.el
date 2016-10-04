c2f2c09ac92f0d48fa8bfbaff4c0770762c80001
Alexandru Nedelcu <noreply@alexn.org>
2016-10-04 14:45:27 +0300

    Edit ocaml

--- emacs.el
+++ emacs.el
@@ -1,3 +1,4 @@
+
 (add-to-list 'load-path "~/.emacs.d/elpa/")
 
 (require 'package)
--- settings/ocaml.el
+++ settings/ocaml.el
@@ -1,10 +1,19 @@
+(defvar ocaml-packages '(tuareg
+                         merlin
+                         ocp-indent))
+
+(dolist (p ocaml-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
 ;(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 ;(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
 
-;(require 'ocp-indent)
-;(require 'merlin)
+(require 'ocp-indent)
+(require 'merlin)
+(require 'tuareg)
 
-;(add-hook 'tuareg-mode-hook 'merlin-mode)
-;(add-hook 'haskell-mode-hook 'intero-mode)
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+(add-hook 'tuareg-mode-hook 'ocp-indent)
 
 
