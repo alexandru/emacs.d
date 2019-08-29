4ed8232d8b8fa57c7a99b2f1e0f7bbbddebc42f6
Alexandru Nedelcu <noreply@alexn.org>
2019-08-29 15:10:06 +0300

    Remove clojure packages

--- emacs.el
+++ emacs.el
@@ -15,8 +15,8 @@
 (load-file "~/.emacs.d/settings/0-set-path.el")
 (load-file "~/.emacs.d/settings/1-customizations.el")
 (load-file "~/.emacs.d/settings/2-graphics.el")
+(load-file "~/.emacs.d/settings/3-basic.el")
 
-(load-file "~/.emacs.d/settings/clojure-mode.el")
 (load-file "~/.emacs.d/settings/emoji.el")
 (load-file "~/.emacs.d/settings/epa.el")
 (load-file "~/.emacs.d/settings/fira-code.el")
